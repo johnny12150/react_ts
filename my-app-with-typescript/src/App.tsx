52bdffe3133ca3b1818b164f83e609a17e27efd4
wade <johnny12150@gmail.com>
2022-01-16 00:24:13 +0800

    Custom grid cell color

--- my-app-with-typescript/src/App.tsx
+++ my-app-with-typescript/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {FunctionComponent, ReactNode} from 'react';
 import { Calendar, Views, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment'
 import "react-big-calendar/lib/css/react-big-calendar.css";
@@ -14,9 +14,20 @@ const myEvents = [
 ]
 
 // 抓出所有view的種類
-let allViews = Object.values(Views)
+// let allViews = Object.values(Views)
 // https://stackoverflow.com/a/69198602
-console.log(allViews.map(k => Views[k as keyof typeof Views]))
+// console.log(allViews.map(k => Views[k as keyof typeof Views]))
+
+// component type, https://stackoverflow.com/a/57062512
+// children type, https://stackoverflow.com/a/57253387
+//  Object destructuring的type設定, https://flaviocopes.com/typescript-object-destructuring/
+// React cloneElment type, https://stackoverflow.com/a/42261933
+const ColoredDateCellWrapper: FunctionComponent = ({children}: {children?: ReactNode}) =>
+    React.cloneElement(React.Children.only(children) as React.ReactElement<any>, {
+      style: {
+        backgroundColor: 'lightgray',
+      },
+    })
 
 function App() {
   return (
@@ -26,6 +37,10 @@ function App() {
     defaultView="month"
     events={myEvents}
     style={{ height: "100vh" }}
+    components={{
+        // 會影響到有grid cell的
+        timeSlotWrapper: ColoredDateCellWrapper,
+    }}
     />
   );
 }
