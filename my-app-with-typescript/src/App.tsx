537a8153f30d89a44f31d66d306401c772c8a80c
wade <johnny12150@gmail.com>
2022-01-16 16:23:30 +0800

    Add other kinds of calendar views

--- my-app-with-typescript/src/App.tsx
+++ my-app-with-typescript/src/App.tsx
@@ -1,21 +1,32 @@
 import React from 'react'
-import FullCalendar from '@fullcalendar/react' // must go before plugins
+import FullCalendar, {ToolbarInput} from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
 import CSS from 'csstype'
 
 // height只能在calendar內控制
+// https://www.kindacode.com/snippet/react-typescript-using-inline-styles-correctly/
 const myCal: CSS.Properties = {
   marginTop: '30px',
   marginLeft: '30px',
   marginRight: '30px',
 }
 
+const myHear: ToolbarInput = {
+    right:'dayGridMonth, timeGridWeek, listWeek'
+}
+
 function App() {
   return (
       <div style={myCal}>
         <FullCalendar
-            plugins={[ dayGridPlugin ]}
+            plugins={[ dayGridPlugin, timeGridPlugin, listPlugin ]}
             initialView="dayGridMonth"
+            headerToolbar={{
+                right:'dayGridMonth, timeGridWeek, listWeek'
+            }}
+            // headerToolbar={myHear}
             height={'90vh'}
         />
       </div>
